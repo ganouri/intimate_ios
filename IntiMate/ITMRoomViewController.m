c05c18874191711cad078b5676856409c4d25d56
Kastet <kastetdev@gmail.com>
2013-07-17 21:33:41 +1000

    Basic navigation implemented;

---
+++ IntiMate/ITMRoomViewController.m
@@ -0,0 +1,38 @@
+//
+//  ITMRoomViewController.m
+//  IntiMate
+//
+//  Created by Konstantin Kim on 17/07/13.
+//  Copyright (c) 2013 IntiMate. All rights reserved.
+//
+
+#import "ITMRoomViewController.h"
+
+@interface ITMRoomViewController ()
+
+@end
+
+@implementation ITMRoomViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
