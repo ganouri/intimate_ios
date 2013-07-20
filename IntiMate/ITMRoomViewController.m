59c6fb2e610442a25331450ef3624ecce888e476
Kastet <kastetdev@gmail.com>
2013-07-21 01:24:53 +1000

    Photo Picker

--- IntiMate/ITMRoomViewController.m
+++ IntiMate/ITMRoomViewController.m
@@ -8,6 +8,7 @@
 
 #import "ITMRoomViewController.h"
 #import "UIBubbleTableView.h"
+#import "UIActionSheet+MKBlockAdditions.h"
 
 @interface ITMRoomViewController () <UIBubbleTableViewDataSource> {
     UIBubbleTableView *_bubbleView;
@@ -35,6 +36,10 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
     
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd
+                                                                                           target:self
+                                                                                           action:@selector(addButtonClicked)];
+    
     _bubbleView = [[UIBubbleTableView alloc] initWithFrame:self.view.bounds];
     _bubbleView.bubbleDataSource = self;
     _bubbleView.showAvatars = YES;
@@ -56,10 +61,18 @@
 - (NSBubbleData *)bubbleTableView:(UIBubbleTableView *)tableView dataForRow:(NSInteger)row {
     NSBubbleType type = row%2 == 0 ? BubbleTypeMine : BubbleTypeSomeoneElse;
     NSBubbleData *heyBubble = nil;
-    // text only
-    heyBubble = [NSBubbleData dataWithText:_dataSource[row]
-                                      date:[NSDate date]
-                                      type:type];
+    
+    if ([_dataSource[row] isKindOfClass:[UIImage class]]) {
+        heyBubble = [NSBubbleData dataWithImage:_dataSource[row]
+                                           date:[NSDate date]
+                                           type:type];
+    } else {
+        // text only
+        heyBubble = [NSBubbleData dataWithText:_dataSource[row]
+                                          date:[NSDate date]
+                                          type:type];
+    }
+    
     if (type == BubbleTypeSomeoneElse) {
         heyBubble.avatar = [UIImage imageNamed:@"profile_picture_generic"];
     } else {
@@ -68,6 +81,22 @@
 
 
     return heyBubble;
+}
+
+#pragma mark - Add button clicked
+
+- (void)addButtonClicked {
+    [UIActionSheet photoPickerWithTitle:@"Attach File"
+                             showInView:self.view
+                              presentVC:self
+                          onPhotoPicked:^(UIImage *chosenImage) {
+                              
+//                              NSData *data = UIImageJPEGRepresentation(chosenImage, 0.2);
+                              [_dataSource addObject:chosenImage];
+                              [_bubbleView reloadData];
+                          } onCancel:^{
+                              
+                          }];
 }
 
 
